ba12cb5db9c8e99cd79ec26f1a01e51220010d9a
Tereza Vrchovinová <tereza.vrchovinova@gmail.com>
2025-04-08 19:24:03 +0200

    fix: update SpeedInsights import path to use the correct package

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { SpeedInsights } from '@vercel/speed-insights/next'
+import { SpeedInsights } from '@vercel/speed-insights/react'
 
 // Global styles & config
 import './i18n'
