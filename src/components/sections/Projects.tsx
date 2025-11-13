7dee2b1be7ef11b205d93e5fd717d5df6c039a6c
Tereza Vrchovinová <tereza.vrchovinova@gmail.com>
2025-11-13 20:09:44 +0100

    refactor: reorganize project thumbnails into desktop and mobile directories, update asset imports in Projects component

--- src/components/sections/Projects.tsx
+++ src/components/sections/Projects.tsx
@@ -1,24 +1,39 @@
 import { useTranslation } from 'react-i18next'
 
-// Project assets
-import kynezImg from '../../assets/project_thumbnails/kynez.webp'
-import variousBannersImg from '../../assets/project_thumbnails/various_banners.webp'
-import saleskitImg from '../../assets/project_thumbnails/saleskit.webp'
-import ditaImg from '../../assets/project_thumbnails/dita.webp'
-import maastrichtImg from '../../assets/project_thumbnails/maastricht.webp'
-import letItRollImg from '../../assets/project_thumbnails/let_it_roll.webp'
-import newtoniaImg from '../../assets/project_thumbnails/newtonia.webp'
-import brejchaImg from '../../assets/project_thumbnails/brejcha.webp'
-import renaissanceImg from '../../assets/project_thumbnails/mona_lisa.webp'
-import deathvalleyImg from '../../assets/project_thumbnails/death_valley.webp'
-import jurassicImg from '../../assets/project_thumbnails/jurassic_adventure.webp'
-import tmobileImg from '../../assets/project_thumbnails/t-mobile.webp'
+// Project assets - Desktop
+import kynezImgDesktop from '../../assets/project_thumbnails/desktop/kynez.webp'
+import variousBannersImgDesktop from '../../assets/project_thumbnails/desktop/various_banners.webp'
+import saleskitImgDesktop from '../../assets/project_thumbnails/desktop/saleskit.webp'
+import ditaImgDesktop from '../../assets/project_thumbnails/desktop/dita.webp'
+import maastrichtImgDesktop from '../../assets/project_thumbnails/desktop/maastricht.webp'
+import letItRollImgDesktop from '../../assets/project_thumbnails/desktop/let_it_roll.webp'
+import newtoniaImgDesktop from '../../assets/project_thumbnails/desktop/newtonia.webp'
+import brejchaImgDesktop from '../../assets/project_thumbnails/desktop/brejcha.webp'
+import renaissanceImgDesktop from '../../assets/project_thumbnails/desktop/mona_lisa.webp'
+import deathvalleyImgDesktop from '../../assets/project_thumbnails/desktop/death_valley.webp'
+import jurassicImgDesktop from '../../assets/project_thumbnails/desktop/jurassic_adventure.webp'
+import tmobileImgDesktop from '../../assets/project_thumbnails/desktop/t-mobile.webp'
+
+// Project assets - Mobile
+import kynezImgMobile from '../../assets/project_thumbnails/mobile/kynez.webp'
+import variousBannersImgMobile from '../../assets/project_thumbnails/mobile/various_banners.webp'
+import saleskitImgMobile from '../../assets/project_thumbnails/mobile/saleskit.webp'
+import ditaImgMobile from '../../assets/project_thumbnails/mobile/dita.webp'
+import maastrichtImgMobile from '../../assets/project_thumbnails/mobile/maastricht.webp'
+import letItRollImgMobile from '../../assets/project_thumbnails/mobile/let_it_roll.webp'
+import newtoniaImgMobile from '../../assets/project_thumbnails/mobile/newtonia.webp'
+import brejchaImgMobile from '../../assets/project_thumbnails/mobile/brejcha.webp'
+import renaissanceImgMobile from '../../assets/project_thumbnails/mobile/mona_lisa.webp'
+import deathvalleyImgMobile from '../../assets/project_thumbnails/mobile/death_valley.webp'
+import jurassicImgMobile from '../../assets/project_thumbnails/mobile/jurassic_adventure.webp'
+import tmobileImgMobile from '../../assets/project_thumbnails/mobile/t-mobile.webp'
 
 // Types
 interface Project {
   id: number
   key: string
-  image: string
+  imageMobile: string
+  imageDesktop: string
   link: string
 }
 
@@ -31,73 +46,85 @@ const PROJECTS: Project[] = [
   {
     id: 1,
     key: 'kynez',
-    image: kynezImg,
+    imageMobile: kynezImgMobile,
+    imageDesktop: kynezImgDesktop,
     link: 'https://www.behance.net/gallery/222330047/Kynez-Sportswear-brand-identity',
   },
   {
     id: 2,
     key: 'tmobile',
-    image: tmobileImg,
+    imageMobile: tmobileImgMobile,
+    imageDesktop: tmobileImgDesktop,
     link: 'https://www.behance.net/gallery/238307417/T-Mobile-Concept-Campaign',
   },
   {
     id: 3,
     key: 'maastricht',
-    image: maastrichtImg,
+    imageMobile: maastrichtImgMobile,
+    imageDesktop: maastrichtImgDesktop,
     link: 'https://www.behance.net/gallery/199767453/Maastricht-Festival-Visual',
   },
   {
     id: 4,
     key: 'saleskit',
-    image: saleskitImg,
+    imageMobile: saleskitImgMobile,
+    imageDesktop: saleskitImgDesktop,
     link: 'https://www.behance.net/gallery/219839403/Design-for-Saleskit',
   },
   {
     id: 5,
     key: 'various_banners',
-    image: variousBannersImg,
+    imageMobile: variousBannersImgMobile,
+    imageDesktop: variousBannersImgDesktop,
     link: 'https://www.behance.net/gallery/223131257/Banner-Designs-Event-Exhibition-Series',
   },
   {
     id: 6,
     key: 'dita',
-    image: ditaImg,
+    imageMobile: ditaImgMobile,
+    imageDesktop: ditaImgDesktop,
     link: 'https://www.behance.net/gallery/205350295/Dita-Von-Teese-visual',
   },
   {
     id: 7,
     key: 'letitroll',
-    image: letItRollImg,
+    imageMobile: letItRollImgMobile,
+    imageDesktop: letItRollImgDesktop,
     link: 'https://www.behance.net/gallery/196798719/Let-It-Roll-Festival-poster',
   },
   {
     id: 8,
     key: 'newtonia',
-    image: newtoniaImg,
+    imageMobile: newtoniaImgMobile,
+    imageDesktop: newtoniaImgDesktop,
     link: 'https://www.behance.net/gallery/201179759/Game-designNewtonia',
   },
   {
     id: 9,
     key: 'brejcha',
-    image: brejchaImg,
+    imageMobile: brejchaImgMobile,
+    imageDesktop: brejchaImgDesktop,
     link: 'https://www.behance.net/gallery/189009581/Boris-Brejcha-video-spot',
   },
   {
     id: 10,
     key: 'renaissance',
-    image: renaissanceImg,
+    imageMobile: renaissanceImgMobile,
+    imageDesktop: renaissanceImgDesktop,
     link: 'https://www.behance.net/gallery/207746525/The-Great-Masters-of-the-Renaissance-video-spot',
   },
   {
     id: 11,
     key: 'deathvalley',
-    image: deathvalleyImg,
+    imageMobile: deathvalleyImgMobile,
+    imageDesktop: deathvalleyImgDesktop,
     link: 'https://www.behance.net/gallery/200322649/Death-Valley-design',
   },
   {
     id: 12,
     key: 'jurassic',
-    image: jurassicImg,
+    imageMobile: jurassicImgMobile,
+    imageDesktop: jurassicImgDesktop,
     link: 'https://www.behance.net/gallery/188776307/brochure-Jurassic-Adventure',
   },
 ]
@@ -139,16 +166,29 @@ export const Projects = () => {
                 className="group relative aspect-[16/9] overflow-hidden rounded-2xl shadow-md transition-shadow duration-300 ease-in-out hover:shadow-lg"
                 style={{ transitionDelay: `${animationDelay}ms` }}
               >
-                {/* Project Image */}
-                <img
-                  src={project.image}
-                  alt={title}
-                  loading="lazy"
-                  width={800}
-                  height={450}
-                  decoding="async"
-                  className="w-full h-full object-cover transform will-change-transform transition-transform duration-300 ease-in-out group-hover:scale-105 group-hover:-translate-y-1"
-                />
+                {/* Project Image - Desktop and Mobile */}
+                <picture>
+                  <source
+                    media="(max-width: 639px)"
+                    srcSet={project.imageMobile}
+                    type="image/webp"
+                  />
+                  <source
+                    media="(min-width: 640px)"
+                    srcSet={project.imageDesktop}
+                    type="image/webp"
+                  />
+                  <img
+                    src={project.imageDesktop}
+                    alt={title}
+                    loading="lazy"
+                    width={800}
+                    height={450}
+                    decoding="async"
+                    fetchPriority="low"
+                    className="w-full h-full object-cover transform will-change-transform transition-transform duration-300 ease-in-out group-hover:scale-105 group-hover:-translate-y-1"
+                  />
+                </picture>
 
                 {/* Overlay on Hover */}
                 <div
